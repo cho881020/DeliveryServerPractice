c6457eceb6762d1f195e2f27f036e51d07bdd63c
test <test1>
2019-01-05 11:27:00 +0900

    로그인 버튼이 눌리면, 서버에 로그인 요청 - Connect Server 클래스에 작성한 메소드를 활용! - new JsonReponseHandler를 이용해서 응답이 돌아오면 할일을 정의

--- app/src/main/java/tjeit/kr/deliveryserverpractice/LoginActivity.java
+++ app/src/main/java/tjeit/kr/deliveryserverpractice/LoginActivity.java
@@ -1,9 +1,21 @@
 package tjeit.kr.deliveryserverpractice;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import org.json.JSONObject;
+
+import tjeit.kr.deliveryserverpractice.utils.ConnectServer;
 
 public class LoginActivity extends BaseActivity {
+
+    private android.widget.EditText userIdEdt;
+    private android.widget.EditText passwordEdt;
+    private android.widget.Button signUpBtn;
+    private android.widget.Button loginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +28,21 @@ public class LoginActivity extends BaseActivity {
 
     @Override
     public void setupEvents() {
-
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                로그인 버튼이 눌리면?
+//                서버에 아이디 비번을 들고 /auth로 접속해서 로그인 결과 물어보기.
+                ConnectServer.postRequestLoign(mContext, userIdEdt.getText().toString(),
+                        passwordEdt.getText().toString(), new ConnectServer.JsonResponseHandler() {
+                            @Override
+                            public void onResponse(JSONObject json) {
+//                                서버에서 돌려주는 응답 처리.
+                                Log.d("로그인 서버응답",json.toString());
+                            }
+                        });
+            }
+        });
     }
 
     @Override
@@ -26,6 +52,10 @@ public class LoginActivity extends BaseActivity {
 
     @Override
     public void bindViews() {
+        this.loginBtn = (Button) findViewById(R.id.loginBtn);
+        this.signUpBtn = (Button) findViewById(R.id.signUpBtn);
+        this.passwordEdt = (EditText) findViewById(R.id.passwordEdt);
+        this.userIdEdt = (EditText) findViewById(R.id.userIdEdt);
 
     }
 }
