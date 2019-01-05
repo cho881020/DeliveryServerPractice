6b7a0447e3aad1f97e77ed35b0d6acb3ecb82e90
test <test1>
2019-01-05 14:11:31 +0900

    로그인한 사용자의 프로필 사진도 띄움

--- app/src/main/java/tjeit/kr/deliveryserverpractice/MainActivity.java
+++ app/src/main/java/tjeit/kr/deliveryserverpractice/MainActivity.java
@@ -3,6 +3,8 @@ package tjeit.kr.deliveryserverpractice;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+
 import de.hdodenhof.circleimageview.CircleImageView;
 import tjeit.kr.deliveryserverpractice.datas.User;
 
@@ -32,6 +34,8 @@ public class MainActivity extends BaseActivity {
         mUser = (User) getIntent().getSerializableExtra("로그인한사람");
         String welcomMessage = String.format("%s님,\n오늘도 힘내주세요!",mUser.getName());
         welcomeMsgTxt.setText(welcomMessage);
+
+        Glide.with(mContext).load(mUser.getProfile_image()).into(profileImageView);
     }
 
     @Override
