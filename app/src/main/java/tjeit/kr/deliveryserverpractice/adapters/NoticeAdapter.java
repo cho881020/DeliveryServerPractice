3b431e5855f9b2e8e8b550612be2b7bdb1ba41f9
Hottory <gkffndnl123@naver.com>
2019-01-05 15:25:51 +0900

    공지 목록 띄우는 작업중

---
+++ app/src/main/java/tjeit/kr/deliveryserverpractice/adapters/NoticeAdapter.java
@@ -0,0 +1,18 @@
+package tjeit.kr.deliveryserverpractice.adapters;
+
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+import java.util.List;
+
+import tjeit.kr.deliveryserverpractice.R;
+import tjeit.kr.deliveryserverpractice.datas.Announcement;
+
+public class NoticeAdapter extends ArrayAdapter<Announcement> {
+
+
+    public NoticeAdapter(Context context, List<Announcement>list){
+
+        super(context,R.layout.notice_list_item, list);
+    }
+}
