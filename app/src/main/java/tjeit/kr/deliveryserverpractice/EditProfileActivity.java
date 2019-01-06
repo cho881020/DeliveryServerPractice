be5de310a5061c3b23ce9bf65e538eb4ba84170f
Hottory <gkffndnl123@naver.com>
2019-01-06 10:36:25 +0900

    은행 목록 레이아웃 xml 파일 작성

--- app/src/main/java/tjeit/kr/deliveryserverpractice/EditProfileActivity.java
+++ app/src/main/java/tjeit/kr/deliveryserverpractice/EditProfileActivity.java
@@ -9,10 +9,15 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import tjeit.kr.deliveryserverpractice.datas.Bank;
 import tjeit.kr.deliveryserverpractice.utils.ConnectServer;
 
 public class EditProfileActivity extends BaseActivity {
+
+    List<Bank> bankList = new ArrayList<Bank>();
 
     private android.widget.Spinner bankSpinner;
     private android.widget.EditText bankAccountEdt;
@@ -64,6 +69,8 @@ public class EditProfileActivity extends BaseActivity {
                             Bank bank = Bank.getBankFromJson(bankJson);
 
 
+
+                            bankList.add(bank);
 
 
                         }
---
+++ app/src/main/java/tjeit/kr/deliveryserverpractice/adapters/BankSpinnerAdapter.java
@@ -0,0 +1,7 @@
+package tjeit.kr.deliveryserverpractice.adapters;
+
+public class BankSpinnerAdapter {
+
+
+
+}
