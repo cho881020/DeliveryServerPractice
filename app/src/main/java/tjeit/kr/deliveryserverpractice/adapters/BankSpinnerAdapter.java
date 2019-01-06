4e84d8246768cf9a418e7a7851704a87a5db74d1
test <test1>
2019-01-06 10:59:38 +0900

    스피너 어댑터 연결 작업

---
+++ app/src/main/java/tjeit/kr/deliveryserverpractice/adapters/BankSpinnerAdapter.java
@@ -0,0 +1,69 @@
+package tjeit.kr.deliveryserverpractice.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+import tjeit.kr.deliveryserverpractice.R;
+import tjeit.kr.deliveryserverpractice.datas.Bank;
+
+public class BankSpinnerAdapter extends ArrayAdapter<Bank> {
+
+    Context mContext;
+    List<Bank> mList;
+    LayoutInflater inf;
+
+    public BankSpinnerAdapter(Context context, List<Bank> list){
+        super(context, R.layout.bank_spinner_list_item, list);
+
+        this.mContext = context;
+        this.mList = list;
+        this.inf = LayoutInflater.from(mContext);
+    }
+
+    @Override
+    public View getDropDownView(int position, View convertView, ViewGroup parent) {
+        View row = convertView;
+
+        if(row == null){
+            row = inf.inflate(R.layout.bank_spinner_list_item, null);
+        }
+
+        ImageView logImg = row.findViewById(R.id.logImg);
+        TextView bankNameTxt = row.findViewById(R.id.bankNameTxt);
+
+        Bank data = mList.get(position);
+
+        Glide.with(mContext).load(data.getLogo()).into(logImg);
+        bankNameTxt.setText(data.getName());
+
+        return row;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View row = convertView;
+
+        if(row == null){
+            row = inf.inflate(R.layout.bank_spinner_list_item, null);
+        }
+
+        ImageView logImg = row.findViewById(R.id.logImg);
+        TextView bankNameTxt = row.findViewById(R.id.bankNameTxt);
+
+        Bank data = mList.get(position);
+
+        Glide.with(mContext).load(data.getLogo()).into(logImg);
+        bankNameTxt.setText(data.getName());
+
+        return row;
+    }
+}
--- app/src/main/java/tjeit/kr/deliveryserverpractice/adapters/bankSpinnerAdapter.java
+++
@@ -1,4 +0,0 @@
-package tjeit.kr.deliveryserverpractice.adapters;
-
-public class bankSpinnerAdapter {
-}
