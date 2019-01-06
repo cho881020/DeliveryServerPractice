60692782e4eda2d7c816dbea8efc293e3f2c6107
test <test1>
2019-01-06 15:02:42 +0900

    아파트 JSON 파싱 Aparment 클래스 기능 완성

--- app/src/main/java/tjeit/kr/deliveryserverpractice/datas/Apartment.java
+++ app/src/main/java/tjeit/kr/deliveryserverpractice/datas/Apartment.java
@@ -1,7 +1,25 @@
 package tjeit.kr.deliveryserverpractice.datas;
 
+import org.json.JSONObject;
+
 import java.io.Serializable;
 
 public class Apartment implements Serializable {
+    private int id;
+    private String name;
+    private double latitude;
+    private double longitude;
+
+//    JSON -> 객체로 변환하는 static 메소드
+    public static Apartment getApartmentFromJson(JSONObject apartJson){
+        Apartment apartment = new Apartment();
+
+        apartment.setId(apartJson.getInt("id"));
+        apartment.setName(apartJson.getString("name"));
+        apartment.setLatitude(apartJson.getDouble("latitude"));
+
+        return apartment;
+    }
+
 
 }
