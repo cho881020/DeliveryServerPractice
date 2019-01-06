7e34ae3d3b51ac35b048f6f0bdebf948796add54
Daeki <kimdaeki@gmail.com>
2019-01-06 14:46:20 +0900

    서울을 지도의 가운데로 + 마커를 추가

--- app/src/main/java/tjeit/kr/deliveryserverpractice/ApartListActivity.java
+++ app/src/main/java/tjeit/kr/deliveryserverpractice/ApartListActivity.java
@@ -3,9 +3,12 @@ package tjeit.kr.deliveryserverpractice;
 import android.app.FragmentManager;
 import android.os.Bundle;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class ApartListActivity extends BaseActivity {
 
@@ -31,6 +34,23 @@ public class ApartListActivity extends BaseActivity {
         mapFragment.getMapAsync(new OnMapReadyCallback() {
             @Override
             public void onMapReady(GoogleMap googleMap) {
+
+
+                LatLng seoul = new LatLng(37.56, 126.97);
+
+//                지도에 위치를 지정하고 마커를 추가
+                MarkerOptions markerOptions = new MarkerOptions();
+                markerOptions.position(seoul);
+                markerOptions.title("서울");
+                markerOptions.snippet("한국의 수도입니다.");
+                googleMap.addMarker(markerOptions);
+
+//               지도의 가운데점을 서울로 미리 세팅.
+                googleMap.moveCamera(CameraUpdateFactory.newLatLng(seoul));
+
+//                지도의 줌 레벨을 세팅
+                googleMap.animateCamera(CameraUpdateFactory.zoomTo(15));
+
 
             }
         });
