2ba01c254f470f3ffd5e5ed0b5f030c199e20ce5
test <test1>
2019-01-06 13:58:04 +0900

    지도 작업중

---
+++ app/src/main/java/tjeit/kr/deliveryserverpractice/ApartListActivity.java
@@ -0,0 +1,32 @@
+package tjeit.kr.deliveryserverpractice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ApartListActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_apart_list);
+
+        bindViews();
+        setupEvents();
+        setValues();
+    }
+
+    @Override
+    public void setupEvents() {
+
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+
+    @Override
+    public void bindViews() {
+
+    }
+}
