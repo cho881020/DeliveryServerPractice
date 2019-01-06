04d679b2ce9fc6016251150a62995f6c303386d6
test <test1>
2019-01-06 11:48:17 +0900

    FCM 코어 라이브러리 Gradle 설치

--- app/src/main/java/tjeit/kr/deliveryserverpractice/adapters/BankSpinnerAdapter.java
+++ app/src/main/java/tjeit/kr/deliveryserverpractice/adapters/BankSpinnerAdapter.java
@@ -53,7 +53,7 @@ public class BankSpinnerAdapter extends ArrayAdapter<Bank> {
         View row = convertView;
 
         if(row == null){
-            row = inf.inflate(R.layout.bank_spinner_list_item, null);
+            row = inf.inflate(R.layout.bank_spinner_selected_item, null);
         }
 
         ImageView logImg = row.findViewById(R.id.logImg);
